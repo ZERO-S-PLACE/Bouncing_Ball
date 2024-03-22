124b72bff71691b3b0ce283f5aba3e0f46095503
zero-s <0-s@wp.pl>
2024-03-22 13:11:06 +0100

    start project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.zeros.bouncy_balls {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.zeros.bouncy_balls to javafx.fxml;
+    exports org.zeros.bouncy_balls;
+}
---
+++ src/main/java/org/zeros/bouncy_balls/HelloController.java
@@ -0,0 +1,14 @@
+package org.zeros.bouncy_balls;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
